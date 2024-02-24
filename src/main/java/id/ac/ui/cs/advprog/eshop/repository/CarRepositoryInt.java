78bf8c4716f50dc32e636867ac7c8e1aee70bb70
HilmyAmmar <hilmyammardarmawan17@gmail.com>
2024-02-24 18:26:25 +0700

    implement DIP

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/CarRepositoryInt.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/CarRepositoryInt.java
@@ -4,6 +4,7 @@ import id.ac.ui.cs.advprog.eshop.model.Car;
 
 import java.util.Iterator;
 
+// implemented OCP
 public interface CarRepositoryInt {
     Car create(Car car);
     Iterator<Car> findAll();
